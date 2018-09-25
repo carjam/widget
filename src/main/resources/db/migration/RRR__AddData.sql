25fb52f2254a5c978126981150b907ad7c675ea1
jamescarson <jamescarson3rd@gmail.com>
2018-09-24 17:04:36 -0700

    Adds idempotent migration for data addition.

---
+++ src/main/resources/db/migration/RRR__AddData.sql
@@ -0,0 +1,31 @@
+TRUNCATE TABLE widget;
+
+INSERT INTO widget(name)
+VALUES('TestName1');
+
+INSERT INTO widget(name)
+VALUES('TestName2');
+
+INSERT INTO widget(name)
+VALUES('TestName3');
+
+INSERT INTO widget(name)
+VALUES('TestName4');
+
+INSERT INTO widget(name)
+VALUES('TestName5');
+
+INSERT INTO widget(name)
+VALUES('TestName6');
+
+INSERT INTO widget(name)
+VALUES('TestName7');
+
+INSERT INTO widget(name)
+VALUES('TestName8');
+
+INSERT INTO widget(name)
+VALUES('TestName9');
+
+INSERT INTO widget(name)
+VALUES('TestName10');
